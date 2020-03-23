3f0384b28bbcf03c35a12ed309ddf1aa526170d8
Timon Emken <emken@chalmers.se>
2020-03-23 16:40:38 +0100

    Remove using namespaces in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,20 +4,17 @@
 
 #include "example.hpp"
 
-using namespace std;
-using namespace std::chrono;
-
 int main(int argc, char *argv[])
 {
 	//Starting time
-	    high_resolution_clock::time_point tStart = high_resolution_clock::now();
+	    std::chrono::high_resolution_clock::time_point time_start = std::chrono::high_resolution_clock::now();
 
 	PrintSomething(argv[1]);
 
 	//Ending time and computing time
-		high_resolution_clock::time_point tEnd = high_resolution_clock::now();
-		double durationTotal =1e-6*duration_cast<microseconds>( tEnd - tStart ).count();
-		cout <<"\nProcessing Time:\t"<< durationTotal<<"s ("<< floor(durationTotal/3600.0)<<":"<<floor(fmod(durationTotal/60.0,60.0))<<":"<<floor(fmod(durationTotal,60.0))<<":"<<floor(fmod(1000*durationTotal,1000.0))<<")."<<endl
-		<<"##############################"<<endl;
+		std::chrono::high_resolution_clock::time_point time_end = std::chrono::high_resolution_clock::now();
+		double durationTotal =1e-6*std::chrono::duration_cast<std::chrono::microseconds>( time_end - time_start ).count();
+		std::cout <<"\nProcessing Time:\t"<< durationTotal<<"s ("<< floor(durationTotal/3600.0)<<":"<<floor(fmod(durationTotal/60.0,60.0))<<":"<<floor(fmod(durationTotal,60.0))<<":"<<floor(fmod(1000*durationTotal,1000.0))<<")."<<std::endl
+		<<"##############################"<<std::endl;
 	return 0;
 }
